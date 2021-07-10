8fd46935000c222620b1bc3975869544d2420746
moinulhossainmahim <moinulhossainmahim@gmail.com>
2021-07-10 21:51:15 +0600

    Added auth0 login and logout authentication

--- src/index.js
+++ src/index.js
@@ -5,12 +5,19 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { GithubProvider } from './context/context';
 import { Auth0Provider } from '@auth0/auth0-react';
-
+// dev-oagvql1d.us.auth0.com
+// cis8VHkMtfJKJqRWF5rNTYshsve3t8pv
 ReactDOM.render(
   <React.StrictMode>
-    <GithubProvider>
-      <App />
-    </GithubProvider>
+    <Auth0Provider
+      domain='dev-oagvql1d.us.auth0.com'
+      clientId='cis8VHkMtfJKJqRWF5rNTYshsve3t8pv'
+      redirectUri={window.location.origin}
+    >
+      <GithubProvider>
+        <App />
+      </GithubProvider>
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
